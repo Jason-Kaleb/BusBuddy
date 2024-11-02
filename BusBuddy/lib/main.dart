b082196c78b5dc642fcd534bb9f4d2c1ca2229da
Jason <kyleskaleb56@gmail.com>
2024-11-02 16:28:23 +0200

    Modified Main.dart

--- BusBuddy/lib/main.dart
+++ BusBuddy/lib/main.dart
@@ -59,7 +59,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Bus Buddy',
       theme: ThemeData(
-        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const HomePage(),
